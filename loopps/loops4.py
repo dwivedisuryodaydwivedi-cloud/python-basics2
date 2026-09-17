e37a88fdfb0044a0fda3ac5479d2c6e4921c78b2
Suryoday Dwivedi <dwivedisuryodaydwivedi@gmail.com>
2026-09-18 02:13:49 +0530

    Add loop to print number in reverse order

---
+++ loopps/loops4.py
@@ -0,0 +1,11 @@
+#Print Given number into reverse Order Using Loop 
+num = int(input("Enter Any Number : "))
+
+print("Actual NUmber is : ",num)
+rev = 0
+while(num != 0):
+    rem = num % 10
+    rev = rev*10 + rem
+    num = num // 10
+
+print("Prin Given number into Reverse Order : ",rev)  
