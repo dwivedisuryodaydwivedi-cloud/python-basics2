ca880557b4e63065f917a9624a2999d8fe1d3823
Suryoday Dwivedi <dwivedisuryodaydwivedi@gmail.com>
2026-09-18 02:09:57 +0530

    Create loops3.py to print even numbers
    Add a script to print even numbers up to a given number.

---
+++ loops/loops3.py
@@ -0,0 +1,21 @@
+#Print The Series of Even numbers upto n number
+
+num = int(input("Enter Any Number : "))
+
+'''
+i = 1
+while i<=num:
+    if i % 2 == 0:
+       print(i)
+    i = i + 1
+
+print("Bye.......")    
+'''
+#In Other Way : 
+
+i = 0
+while i<=num:
+    print(i)
+    i = i + 2
+
+print("Bye.......")    
