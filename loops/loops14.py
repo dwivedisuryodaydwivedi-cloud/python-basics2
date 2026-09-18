ca300f6c603bdc83b86e7148217d3e2e68f95258
Suryoday Dwivedi <dwivedisuryodaydwivedi@gmail.com>
2026-09-19 01:54:24 +0530

    Add loop to print odd numbers using continue
    Implement a loop to print odd numbers from 1 to 49 using continue.

---
+++ loops/loops14.py
@@ -0,0 +1,5 @@
+# WAP to print the odd number using continue 
+for i in range(1,50):
+  if i%2 == 0 :
+    continue # cotinue keyword specifically terminate the steps that you want just like here in this code .
+  print(i)
