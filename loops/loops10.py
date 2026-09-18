340f3dcde20b3b8eef6b8ca64bdd688f87f5ca8c
Suryoday Dwivedi <dwivedisuryodaydwivedi@gmail.com>
2026-09-19 01:12:45 +0530

    Implement loop to print list elements
    Add a loop to print elements of a predefined list.

---
+++ loops/loops10.py
@@ -0,0 +1,5 @@
+#printing the given list of element using loop [1,4,9,16,25,36,49,64,81,100]
+nums = [1,4,9,16,25,36,49,64,81,100]
+for i in range(len(nums)): # loop for printing one to 10 number 
+  print(nums[i])
+  
