a68fe351b168747fe4adddff3ca7ab4130629f69
Suryoday Dwivedi <dwivedisuryodaydwivedi@gmail.com>
2026-09-19 00:57:18 +0530

    Implement loop to print squares of numbers
    Add a loop to print squares of numbers from 1 to 10.

---
+++ loops/loops9.py
@@ -0,0 +1,4 @@
+# WAP to print the list of element using loop [1,4,9,16,25,36,49,64,81,100]
+
+for i in range(1,11): # loop for printing one to 10 number 
+  print(i*i) # print square of each number form 1 to 10 
