141cdb6184f561275414d2522c8c2eeee597a94d
Suryoday Dwivedi <dwivedisuryodaydwivedi@gmail.com>
2026-09-19 01:48:15 +0530

    Implement continue statement in loop example
    Add example of 'continue' keyword in loops

---
+++ loops/loops13.py
@@ -0,0 +1,5 @@
+# working of continue in loops 
+for i in range(1,50):
+  if i == 45 :
+    continue # cotinue keyword specifically terminate the steps that you want just like here in this code .
+  print(i)
